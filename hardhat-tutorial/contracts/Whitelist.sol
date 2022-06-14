df18e79b19856ce20b0075829d04bda7241d10ec
sayf-ismail <63899527+sayf-ismail@users.noreply.github.com>
2022-06-14 17:02:23 +0800

    add next.js framework

--- hardhat-tutorial/contracts/Whitelist.sol
+++ hardhat-tutorial/contracts/Whitelist.sol
@@ -2,5 +2,28 @@
 pragma solidity ^0.8.0;
 
 contract Whitelist {
-  
+
+  // maximum number of addresses which can be whitelisted
+  uint8 public maxWhitelistedAddresses;
+
+  // keep track of number of addresses whitelisted until now
+  uint8 public numAddressesWhitelisted;
+
+  // bool is already initialised to false
+  mapping(address => bool) public whitelistedAddresses;
+
+  constructor(uint8 _maxWhitelistedAddresses) {
+    maxWhitelistedAddresses = _maxWhitelistedAddresses;
+  }
+
+  function addAddressToWhitelist() public {
+    // msg.sender is the address of the user who called this function
+    require(!whitelistedAddresses[msg.sender], "Sender already in the whitelist");
+    require(numAddressesWhitelisted < maxWhitelistedAddresses, "Max limit reached");
+    whitelistedAddresses[msg.sender] = true;
+    numAddressesWhitelisted += 1;
+  }
+
+
+
 }
---
+++ hardhat-tutorial/scripts/deploy.js
@@ -0,0 +1,21 @@
+const {ethers} = require("hardhat")
+
+async function main() {
+  // create the instance
+  const whitelistContract = await ethers.getContractFactory("Whitelist");
+
+  // input 10 into the constructor
+  const deployedWhitelistContract = await whitelistContract.deploy(10);
+
+  await deployedWhitelistContract.deployed();
+
+  console.log("Whitelist Contract Address", deployedWhitelistContract.address);
+}
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+
+    // Whitelist Contract Address 0x46680152B7Edfd31E9aF8620cC95E91a8B8B935A
